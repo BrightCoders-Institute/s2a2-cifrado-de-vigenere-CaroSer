30db7ad087bc7fb0f8a4e9aaccb3cb561e8cd2d6
Carolina Serrano <adrianaserrano185@outlook.com>
2024-01-04 23:32:04 -0600

    Implement correct style syntax with rubocop

--- spec/vigenere_cipher_spec.rb
+++ spec/vigenere_cipher_spec.rb
@@ -1,22 +1,21 @@
+# frozen_string_literal: true
+
 require_relative '../lib/vigenere_cipher'
 require 'rspec'
 
 describe VigenereCipher do
-  context "Decode function" do
-    it "returns decoded message" do
-      
-cipher=VigenereCipher.new
-decoded= cipher.decode("ROVWSOIV","PASSWORD")
-expect(decoded).to eq("CODEWARS")
+  context 'Decode function' do
+    it 'returns decoded message' do
+      cipher = VigenereCipher.new
+      decoded = cipher.decode('ROVWSOIV', 'PASSWORD')
+      expect(decoded).to eq('CODEWARS')
     end
   end
-  context "Encode function" do
-    it "returns encoded message" do
-      
-cipher=VigenereCipher.new
-encoded= cipher.encode("CODEWARS","PASSWORD")
-expect(encoded).to eq("ROVWSOIV")
+  context 'Encode function' do
+    it 'returns encoded message' do
+      cipher = VigenereCipher.new
+      encoded = cipher.encode('CODEWARS', 'PASSWORD')
+      expect(encoded).to eq('ROVWSOIV')
     end
-  end 
+  end
 end
-
